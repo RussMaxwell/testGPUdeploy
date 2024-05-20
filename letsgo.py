ba3e1bcfdd85263ae26ff588b41b6507d8ca8a7d
RussMaxwell@gmail.com <RussMaxwell@gmail.com>
2024-05-20 19:44:01 -0400

    first commit

---
+++ letsgo.py
@@ -0,0 +1,5 @@
+# Script to demo a simply python script
+# that is added to build pipeline and added to release pipeline
+# finally release pipeline deploys to external gpu server
+
+print("This file was successfully deployed via Azure DevOps to the GPU Server")
